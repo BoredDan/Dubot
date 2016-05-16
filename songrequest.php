94c3ce092530d81fcfc19830b902a07c8ec4abc4
Daniel Kelly <dlpkelly@hotmail.com>
2016-05-16 18:31:57 -0400

    Initial file setup

--- songrequest.php
+++ songrequest.php
@@ -1,12 +1,3 @@
 <?php
-	$url="https://api.dubtrack.fm";
-	$roomuri = "boreddan-test";
-	$getroom = "/room/" . $roomuri;
-	echo $url . $getroom;
-	
-	$ch = curl_init($url.$getroom);
-	
-	curl_exec($ch);
-	
-	curl_close($ch);
+	require("dubot-utility");
 ?>
