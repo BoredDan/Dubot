125a7205a09726661a499249b3ddbaba9e9e6f14
Daniel Kelly <dlpkelly@hotmail.com>
2016-05-16 23:33:43 -0400

    Added grabbing of room details and id

--- dubot-utility.php
+++ dubot-utility.php
@@ -3,4 +3,22 @@
 		parse_ini_file("dubot.ini"),
 		parse_ini_file("dubot-user.ini")
 	);
+	
+	function roomDetails($room) {
+		global $ini;
+	
+		$ch = curl_init(str_replace("{room}", $room, $ini["api_url"].$ini["api_room"]));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		$json = json_decode(curl_exec($ch), true);
+		
+		curl_close($ch);
+		
+		return $json;
+	}
+	
+	function roomId($room) {
+		$json = roomDetails($room);
+		return $json['data']['_id'];
+	}
 ?>
--- songrequest.php
+++ songrequest.php
@@ -1,3 +1,4 @@
 <?php
 	require("dubot-utility.php");
+	echo roomId($ini["room"]);
 ?>
