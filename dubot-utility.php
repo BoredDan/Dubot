289ddec06e35ebb6c326fa36e88a65ea1be613dd
Daniel Kelly <dlpkelly@hotmail.com>
2016-05-17 15:09:01 -0400

    Created url helper function

--- dubot-utility.php
+++ dubot-utility.php
@@ -4,10 +4,25 @@
 		parse_ini_file("dubot-user.ini")
 	);
 	
+	function getURL($command, $inline_ids = array()) {
+		global $ini;
+		
+		$url = $ini["api_url"].$ini["api_".$command];
+		
+		$search = array_map(function($key){ return "{".$key."}"; }, array_keys($inline_ids));
+		$replace = array_values($inline_ids);
+		
+		$url = str_replace($search, $replace, $url);
+		
+		return $url;
+	}
+	
 	function roomDetails($room) {
 		global $ini;
+		
+		$url = getURL("room_details", array("room" => $room));
 	
-		$ch = curl_init(str_replace("{room}", $room, $ini["api_url"].$ini["api_room"]));
+		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		
 		$json = json_decode(curl_exec($ch), true);
@@ -18,7 +33,13 @@
 	}
 	
 	function roomId($room) {
-		$json = roomDetails($room);
-		return $json['data']['_id'];
+		static $id = null;
+	
+		if($id == null) {
+			$json = roomDetails($room);
+			$id = $json['data']['_id'];
+		}
+		
+		return $id;
 	}
 ?>
