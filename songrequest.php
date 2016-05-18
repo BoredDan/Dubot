2eb082619f243d25506ac27b4cd74040304428dd
Daniel Kelly <dlpkelly@hotmail.com>
2016-05-18 15:18:41 -0400

    Song request returns song name

--- songrequest.php
+++ songrequest.php
@@ -4,7 +4,12 @@
 	init();
 	$queue = queueSong($ini["roomName"], $_GET["song"], "youtube");
 	if($queue["code"] == 200) {
-		echo "Queued ".$_GET["song"]."!";
+		$details = songDetails($queue["data"]["songid"]);
+		if($details["code"] == 200) {
+			echo "Queued \"".$details["data"]["name"]."\"!";
+		} else {
+			echo "Queued ".$_GET["song"]."!";
+		}
 	} else {
 		echo "Failed to queue requested song!";
 	}
